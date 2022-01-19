045395d42e0630baeebdb6fd197759f8db32e3cd
root <mtrhoades0727@gmail.com>
2022-01-18 17:29:30 -1000

    made smaller code

--- index.js
+++ index.js
@@ -8,16 +8,19 @@ function newImage(url, left, bottom){
     return object
 }
 
+// creates function of clicking to sword, staff, or shield to go into bottom inventory box.
 function newItem(url, left, bottom){
     let item = newImage(url, left, bottom)
     item.addEventListener('click', function() {
-        item.remove()
+        item.remove() // removes item from the DOM
         let inventoryItem = document.createElement('img')
         inventoryItem.src = url
-        inventory.append(inventoryItem)
-    })
+        inventory.append(inventoryItem) // putting item in the inventory bottom box
+    });
 }
+
 let inventory = document.createElement('div')
+
 function newInventory(){
 
     inventory.style.position = 'fixed'
@@ -33,20 +36,7 @@ function newInventory(){
     inventory.style.backgroundColor = 'brown'
     document.body.append(inventory)
 }
-function makeInventory() {
-    inventory.style.position = 'fixed'
-    inventory.style.bottom = '0px';
-    inventory.style.left = '0px'
-    inventory.style.width = '100%'
-    inventory.style.height = '100px'
-    inventory.style.display = 'flex'
-    inventory.style.flexDirection = 'row'
-    inventory.style.alignItems = 'center'
-    inventory.style.justifyContent = 'space-evenly'
-    inventory.style.border = '2px solid black'
-    inventory.style.backgroundColor = 'brown'
-    document.body.append(inventory)
-}
+
 
 
 newInventory()
